6760fa23e9511f2171fd427e5490eaa204d57f48
urvisha <urvisha1109@gmail.com>
2023-06-03 10:48:57 +0530

    "3/6/2023"

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -10,6 +10,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+
+        title: Text("Home Screen",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        )),
+      ),
+    );
   }
 }
