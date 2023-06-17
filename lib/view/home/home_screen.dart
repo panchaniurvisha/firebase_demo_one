12045d6186a4d1ca416f14b394b8f9277af71b01
urvisha <urvisha1109@gmail.com>
2023-06-17 11:46:32 +0530

    "17/6/2023"

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../../model/user_model.dart';
 import '../login_page/login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,14 +17,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String? uid;
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-  User? user;
+  FirebaseFirestore firebaseFireStore = FirebaseFirestore.instance;
+
+  UserModel? userModel;
+  getUser() {
+    CollectionReference users = firebaseFireStore.collection('user');
+    users.doc(firebaseAuth.currentUser!.uid).get().then((value) {
+      debugPrint("User Added---->${jsonEncode(value.data())}");
+      userModel = userModelFromJson(jsonEncode(value.data()));
+    }).catchError((error) {
+      debugPrint("Failed to get user: $error");
+    });
+  }
+
   @override
   void initState() {
     // TODO: implement initState
+    getUser();
     super.initState();
-    uid = firebaseAuth.currentUser?.uid;
   }
 
   @override
@@ -41,6 +56,37 @@ class _HomeScreenState extends State<HomeScreen> {
               },
               icon: const Icon(Icons.logout))
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Data Get From Model",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.orangeAccent,
+                  height: 2),
+            ),
+            Text("first_name: ${userModel!.firstName}",
+                style: const TextStyle(
+                    height: 2, fontWeight: FontWeight.bold, fontSize: 18)),
+            Text("last_name: ${userModel!.lastName}",
+                style: const TextStyle(
+                    height: 2, fontWeight: FontWeight.bold, fontSize: 18)),
+            Text("Email: ${userModel!.email}",
+                style: const TextStyle(
+                    height: 2, fontWeight: FontWeight.bold, fontSize: 18)),
+            Text("Phone number: ${userModel!.number}",
+                style: const TextStyle(
+                    height: 2, fontWeight: FontWeight.bold, fontSize: 18)),
+            Text("Id: ${userModel!.id}",
+                style: const TextStyle(
+                    height: 2, fontWeight: FontWeight.bold, fontSize: 18)),
+          ],
+        ),
       ),
     );
   }
--- lib/view/splash/splash_screen.dart
+++ lib/view/splash/splash_screen.dart
@@ -20,9 +20,15 @@ class SplashScreenState extends State<SplashScreen> {
       const Duration(seconds: 5), //arguments duration and action
       () {
         if (FirebaseAuth.instance.currentUser != null) {
-          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const HomeScreen()), (route) => false);
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
+              (route) => false);
         } else {
-          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const LoginScreen()), (route) => false);
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginScreen()),
+              (route) => false);
         }
       },
     );
@@ -38,7 +44,10 @@ class SplashScreenState extends State<SplashScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          FlutterLogo(size: height / 3, style: FlutterLogoStyle.horizontal, curve: Curves.easeInCubic),
+          FlutterLogo(
+              size: height / 3,
+              style: FlutterLogoStyle.horizontal,
+              curve: Curves.easeInCubic),
         ],
       ),
     )));
