56173a76b355779bc76dae2cdc5dc16894896686
urvisha <urvisha1109@gmail.com>
2023-06-20 15:21:50 +0530

    "20/6/2023"

--- lib/res/commen/app_text.dart
+++ lib/res/commen/app_text.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 
 class AppText extends StatelessWidget {
   final String? text;
+  final double? height;
   final double? fontSize;
-  const AppText({super.key, required this.text, this.fontSize});
+  final Color? color;
+  const AppText(
+      {super.key, required this.text, this.fontSize, this.height, this.color});
 
   @override
   Widget build(BuildContext context) {
     return Text(text!,
         style: TextStyle(
-          height: 3,
-          color: Color(0xff000000),
+          height: height ?? 3,
+          color: color ?? Color(0xff000000),
           fontWeight: FontWeight.w500,
           fontSize: fontSize ?? 14,
           fontFamily: "Circular Std",
--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_demo_one/res/commen/app_text.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -30,6 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final ImagePicker picker = ImagePicker();
   XFile? image, galleryImage;
   File? imageFile, captureImage;
+
   getUser() {
     CollectionReference users = firebaseFireStore.collection('user');
     users.doc(firebaseAuth.currentUser!.uid).get().then((value) {
@@ -156,88 +158,150 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
-          child: userModel == null
-              ? const Center(child: CircularProgressIndicator())
-              : Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        pickImage();
-                      },
-                      child: ClipOval(
-                          child: imageFile != null
-                              ? Image.file(
-                                  imageFile!,
-                                  height: 100,
-                                  width: 100,
-                                  fit: BoxFit.cover,
-                                )
-                              : Image.asset(
-                                  "assets/images/profile.png",
-                                  height: 100,
-                                  width: 100,
-                                  fit: BoxFit.cover,
-                                )),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    InkWell(
-                      onTap: () {
-                        pickImageFromGallery();
-                      },
-                      child: ClipOval(
-                          child: captureImage != null
-                              ? Image.file(
-                                  captureImage!,
-                                  height: 100,
-                                  width: 100,
-                                  fit: BoxFit.cover,
-                                )
-                              : Image.asset(
-                                  "assets/images/profile_one.png",
-                                  height: 100,
-                                  width: 100,
-                                  fit: BoxFit.cover,
-                                )),
-                    ),
-                    const Text(
-                      "Data Get From Model",
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.orangeAccent,
-                          height: 2),
-                    ),
-                    Text("first_name: ${userModel!.firstName}",
-                        style: const TextStyle(
-                            height: 2,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18)),
-                    Text("last_name: ${userModel!.lastName}",
-                        style: const TextStyle(
-                            height: 2,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18)),
-                    Text("Email: ${userModel!.email}",
-                        style: const TextStyle(
-                            height: 2,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18)),
-                    Text("Phone number: ${userModel!.number}",
-                        style: const TextStyle(
-                            height: 2,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18)),
-                    Text("Id: ${userModel!.id}",
-                        style: const TextStyle(
-                            height: 2,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18)),
-                  ],
-                )),
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+        child: userModel == null
+            ? const Center(child: CircularProgressIndicator())
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      ClipOval(
+                        child: imageFile != null
+                            ? Image.file(
+                                imageFile!,
+                                height: 100,
+                                width: 100,
+                                fit: BoxFit.cover,
+                              )
+                            : captureImage != null
+                                ? Image.file(
+                                    captureImage!,
+                                    height: 100,
+                                    width: 100,
+                                    fit: BoxFit.cover,
+                                  )
+                                : Image.asset(
+                                    "assets/images/profile.png",
+                                    height: 100,
+                                    width: 100,
+                                    fit: BoxFit.cover,
+                                  ),
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.teal,
+                          shape: BoxShape.circle,
+                        ),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.camera_alt,
+                          ),
+                          onPressed: () => showModalBottomSheet(
+                            isScrollControlled: true,
+                            isDismissible: true,
+                            context: context,
+                            backgroundColor: Colors.transparent,
+                            // barrierColor: Colors.transparent,
+                            builder: (context) => Container(
+                              height: 150,
+                              width: double.infinity,
+                              clipBehavior: Clip.antiAlias,
+                              decoration: const BoxDecoration(
+                                  color: Colors.black26,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(15),
+                                    topRight: Radius.circular(15),
+                                  )),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 20, horizontal: 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      "Profile photo",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(
+                                      height: 20,
+                                    ),
+                                    Row(
+                                      children: [
+                                        Container(
+                                          decoration: BoxDecoration(
+                                              border: Border.all(
+                                                  color: Colors.white12),
+                                              shape: BoxShape.circle),
+                                          child: IconButton(
+                                            onPressed: () => pickImage(),
+                                            icon: const Icon(
+                                              Icons.camera_alt_rounded,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          width: 20,
+                                        ),
+                                        Container(
+                                          decoration: BoxDecoration(
+                                              border: Border.all(
+                                                  color: Colors.white12),
+                                              shape: BoxShape.circle),
+                                          child: IconButton(
+                                            onPressed: () =>
+                                                pickImageFromGallery(),
+                                            icon: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(4.0),
+                                              child: Image.asset(
+                                                "assets/images/gallery_icon.png",
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const AppText(
+                    text: "Data Get From Model",
+                    fontSize: 20,
+                    color: Colors.indigoAccent,
+                  ),
+                  AppText(
+                    text: "first_name: ${userModel!.firstName}",
+                  ),
+                  AppText(
+                    text: "last_name: ${userModel!.lastName}",
+                  ),
+                  AppText(
+                    text: "Email: ${userModel!.email}",
+                  ),
+                  AppText(
+                    text: "Phone number: ${userModel!.number}",
+                  ),
+                  AppText(
+                    text: "Id: ${userModel!.id}",
+                  ),
+                ],
+              ),
+      ),
     );
   }
 }
